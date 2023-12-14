3155d1fb6abd4a24d9de9c59bf2e695548eec27f
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-12-14 15:33:27 +0100

    Remove explicit path in load_sb3.py

--- lr-quadruped-sim-master/load_sb3.py
+++ lr-quadruped-sim-master/load_sb3.py
@@ -61,7 +61,7 @@ from utils.file_utils import get_latest_model, load_all_results
 LEARNING_ALG = "PPO"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '121321105810'
-log_dir = interm_dir + '120323142035'
+log_dir = interm_dir + '121423104920'
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
@@ -79,8 +79,6 @@ env_config['add_noise'] = False
 # get latest model and normalization stats, and plot 
 stats_path = os.path.join(log_dir, "vec_normalize.pkl")
 model_name = get_latest_model(log_dir)
-print("model_name", model_name)
-model_name = r"C:\Users\naeld\OneDrive - epfl.ch\EPFL_STUDIES\MA3 - 2023\Legged robotics\Prog\Project2\quadruped_locomotion\lr-quadruped-sim-master\logs\intermediate_models\120323142035\rl_model_990000_steps.zip"
 print("model_name", model_name)
 
 monitor_results = load_results(log_dir)
